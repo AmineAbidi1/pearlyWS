689350d33fe11f24f3063fdbad4fcb6508e9713b
Amine Abidi <amine.abidi@vimn.com>
2016-02-14 13:12:30 +0100

    adding spark integration

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -20,10 +20,28 @@ class Api::V1::RoomsController < ApplicationController
 
 		# Send request to Zeus
 
+		#create room
+		#resp = JSON.parse(RestClient.post "https://api.ciscospark.com/v1/rooms", :title => "#{user.nickname} + Bookings + #{DateTime.now}", :content_type => :json, :accept => :json, :Authorization => "Bearer NzM0NGFlZmItOTc4Ni00ZDIzLWEzNWQtMjU3NTNjZDA0OWZlMjllMmYxM2EtOWU2")	
+		params1 = {:title => "#{user.nickname} Bookings #{DateTime.now}"}
+		headers = {:Authorization => "Bearer NzM0NGFlZmItOTc4Ni00ZDIzLWEzNWQtMjU3NTNjZDA0OWZlMjllMmYxM2EtOWU2"}
+		resp = JSON.parse(RestClient.post "https://api.ciscospark.com/v1/rooms", params1, headers)
+		roomId = resp["id"]
+
+		params2 = {:roomId => roomId, :text => "Hey #{user.nickname}, your booking in room #{room.title} is confirmed, get there in 10 minutes or you will lose it!"}
+
+
+		RestClient.post "https://api.ciscospark.com/v1/messages", params2, headers
+#   "roomId" : "24aaa2aa-3dcc-11e5-a152-fe34819cdc9a",
+#   "text" : "PROJECT UPDATE - A new project project plan has been published on Box",
+#   "files" : [ "http://www.example.com/images/media.png" ],
+#   "toPersonId" : "Y2lzY29zcGFyazovL3VzL1BFT1BMRS9mMDZkNzFhNS0wODMzLTRmYTUtYTcyYS1jYzg5YjI1ZWVlMmX",
+#   "toPersonEmail" : "julie@example.com"
+# }
 		reservation = Reservation.new(room_id: room.id, user_id: user.id, start_time: start_time, end_time: end_time)
 		if reservation.save!
 			@result = "Booking created!"
 			RestClient.post "https://api.tropo.com/1.0/sessions", :phoneNumber => user.phoneNumber, :room_name => room.title, :token => "74477a6170634b74424f5176666d4954535263735a44424d64564f63675878716a61636474746e44644e4d4e"
+		
 		else
 			@result = "Something wrong happened!"
 		end
